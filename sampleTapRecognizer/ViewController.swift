7b9a5274cf537d6488b7d3ba53b5035dbe1725f4
AI Matsubara <aiai.1116.info@gmail.com>
2016-05-03 15:59:16 +0800

    initial commit

--- sampleTapRecognizer/ViewController.swift
+++ sampleTapRecognizer/ViewController.swift
@@ -14,7 +14,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
 
+    @IBAction func tapView(sender: UITapGestureRecognizer) {
+        print("タップされたよ")
+    }
+    
+    
+    @IBAction func tapColorView(sender: UITapGestureRecognizer) {
+        print("ブルーがタップされたよ")
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
